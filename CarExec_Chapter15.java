6b9b2f4d22141565bcf6288760a30db7c023801c
takumi097 <igataku97614@gmail.com>
2025-01-31 21:47:26 +0900

    kadai_015再提出

--- CarExec_Chapter15.java
+++ CarExec_Chapter15.java
@@ -5,7 +5,7 @@ public class CarExec_Chapter15 {
 	public static void main(String[] args) {
 		
 		Car_Chapter15 gear = new Car_Chapter15(1, 10);
-		Car_Chapter15 speed =new Car_Chapter15(1, 30);
+		
 		
 		gear.gearChange(3);
 		
--- Car_Chapter15.java
+++ Car_Chapter15.java
@@ -2,18 +2,15 @@ package kadai_015;
 
 public class Car_Chapter15 {
 
-	// フィールド（内部データ）
 	private int gear = 1;
 	private int speed = 10;
 	
-	// コンストラクタ（初期化処理）
 	public Car_Chapter15( int gear, int speed ) {
 		this.gear  = gear;
 		this.speed = speed;
 	}
 		
 			
-	// メソッド
 	public void gearChange(int afterGear) {
 		System.out.println("ギア" + gear + "から" + afterGear + "に切り替えました");
 		this.gear = afterGear;
