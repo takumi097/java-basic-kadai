9348c4e09a01d39097e27f7062028949f6003c72
takumi097 <igataku97614@gmail.com>
2025-02-05 21:36:21 +0900

    kadai_026を提出

---
+++ JyankenExec_Chapter26.java
@@ -0,0 +1,13 @@
+package kadai_026;
+
+public class JyankenExec_Chapter26 {
+
+	public static void main(String[] args) {
+		
+		Jyanken_Chapter26 Janken = new Jyanken_Chapter26();
+		
+		Janken.playGame();
+
+	}
+
+}
---
+++ Jyanken_Chapter26.java
@@ -0,0 +1,65 @@
+package kadai_026;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Jyanken_Chapter26 {
+
+	public Scanner scanner = new Scanner(System.in);
+		
+		public String getMyChoice() {
+			System.out.println("自分のじゃんけんの手を入力しましょう");
+			System.out.println("グーはrockのrを入力しましょう");
+			System.out.println("チョキはscissorsのsを入力しましょう");
+			System.out.println("パーはpaperのpを入力しましょう");
+			
+			String choice = scanner.nextLine();
+			
+			while ( !choice.equals("r") && !choice.equals("s") && !choice.equals("p")) {
+				System.out.println("正しく入力してください");
+				choice = scanner.nextLine();
+				
+			}
+			return choice;
+		}
+		
+		public String getRandom() {
+			String[] opponentChoice = { "r", "s", "p"};
+			int i = (int) (Math.random() * 3);
+			return opponentChoice[ i ];
+			
+		}
+		
+		public void playGame() {
+			HashMap<String, String> result = new HashMap<>();
+			
+			result.put( "r", "グー");
+			result.put("s", "チョキ");
+			result.put( "p", "パー");
+			
+			String mychoice = getMyChoice();
+			String opponentChoice = getRandom();
+			
+			
+			System.out.println("自分の手は" + result.get(mychoice) + ", 対戦相手の手は" + result.get(opponentChoice));
+			
+			if (mychoice.equals(opponentChoice)) {
+				System.out.println("あいこです");
+			}
+			else if ((mychoice.equals("r") && opponentChoice.equals("s")) || (mychoice.equals("s") && opponentChoice.equals("p")) || (mychoice.equals("p") && opponentChoice.equals("r"))) {
+				System.out.println("自分の勝ちです");
+			}
+			else {
+				System.out.println("自分の負けです");
+			}
+			
+		}
+		
+		
+		
+		
+
+	
+
+	}
+
